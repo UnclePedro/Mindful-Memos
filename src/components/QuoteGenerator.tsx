246906da6a8b7cdc06f3d0bf034927a2b4126349
Uncle Pedro <peterforsyth10@gmail.com>
2024-10-03 13:58:13 +1000

    Tweak styling and add react-fade

--- src/components/QuoteGenerator.tsx
+++ src/components/QuoteGenerator.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { generateUserId } from "../helpers/userAuthHelper";
+import { Fade } from "react-awesome-reveal";
 
 const url = "https://random-quote-generator-api.vercel.app";
 
@@ -91,106 +92,122 @@ const QuoteGenerator = () => {
   }, []);
 
   return (
-    <div className="lg:m-10">
-      <button
-        onClick={() => setEditUserId(true)}
-        className="text-xs fixed top-4 right-4 p-2 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 transition"
-      >
-        User ID
-      </button>
+    <Fade>
+      <div className="lg:m-10">
+        <button
+          onClick={() => setEditUserId(true)}
+          className="text-xs fixed top-4 right-4 p-2 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 transition"
+        >
+          User ID
+        </button>
 
-      {editUserId && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg relative">
-            <button
-              onClick={() => setEditUserId(false)}
-              className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
-            >
-              ✖
-            </button>
-            <p className="mb-2">User ID:</p>
+        {editUserId && (
+          <Fade duration={300}>
+            <div className="fixed inset-0 bg-black bg-opacity-65 flex justify-center items-center z-50">
+              <div className="bg-white p-6 rounded-lg shadow-lg relative">
+                <button
+                  onClick={() => setEditUserId(false)}
+                  className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
+                >
+                  ✖
+                </button>
+                <p className="mb-2">User ID:</p>
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setUserId(e.target.value);
+                    localStorage.setItem("userId", e.target.value);
+                  }}
+                  value={userId}
+                  className="p-3 rounded-lg text-black bg-slate-200 border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
+                />
+              </div>
+            </div>
+          </Fade>
+        )}
+
+        {/* Display the random quote */}
+        {randomQuote.quote.length > 0 ? (
+          <div className="p-6 rounded-lg shadow-lg bg-white">
+            <h2 className="text-2xl font-semibold mb-2">{randomQuote.quote}</h2>
+            <p className="text-right italic text-gray-700">
+              {randomQuote.author}
+            </p>
+          </div>
+        ) : (
+          <div className="p-6 mt-6 bg-white rounded-lg shadow-lg">
+            <p className="text-gray-500">Loading a random memo...</p>
+          </div>
+        )}
+
+        {/* Section for leaving a new quote */}
+        <div className="p-6 mt-6 bg-white rounded-lg shadow-lg">
+          <h2 className="text-xl font-semibold mb-4">Leave a quote:</h2>
+          <div className="flex flex-col gap-4">
             <input
               type="text"
-              onChange={(e) => {
-                setUserId(e.target.value);
-                localStorage.setItem("userId", e.target.value);
-              }}
-              value={userId}
+              value={newUserQuote.quote}
+              onChange={(e) =>
+                setNewUserQuote({ ...newUserQuote, quote: e.target.value })
+              }
+              placeholder="Enter a new quote"
               className="p-3 rounded-lg text-black bg-slate-200 border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
             />
+            <input
+              type="text"
+              value={newUserQuote.author}
+              onChange={(e) =>
+                setNewUserQuote({ ...newUserQuote, author: e.target.value })
+              }
+              placeholder="Author"
+              className="p-3 rounded-lg text-black bg-slate-200 border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
+            />
+            <button
+              onClick={addQuote}
+              className="p-3 rounded-lg transition-all hover:bg-blue-600 bg-blue-500 text-white font-bold shadow-lg"
+            >
+              Add Quote
+            </button>
           </div>
         </div>
-      )}
 
-      {/* Display the random quote */}
-      <div className="p-6 rounded-lg shadow-lg bg-white">
-        <h2 className="text-2xl font-semibold mb-2">{randomQuote.quote}</h2>
-        <p className="text-right italic text-gray-700">{randomQuote.author}</p>
-      </div>
-
-      {/* Section for leaving a new quote */}
-      <div className="p-6 mt-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-xl font-semibold mb-4">Leave a quote:</h2>
-        <div className="flex flex-col gap-4">
-          <input
-            type="text"
-            value={newUserQuote.quote}
-            onChange={(e) =>
-              setNewUserQuote({ ...newUserQuote, quote: e.target.value })
-            }
-            placeholder="Enter a new quote"
-            className="p-3 rounded-lg text-black bg-slate-200 border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
-          />
-          <input
-            type="text"
-            value={newUserQuote.author}
-            onChange={(e) =>
-              setNewUserQuote({ ...newUserQuote, author: e.target.value })
-            }
-            placeholder="Author"
-            className="p-3 rounded-lg text-black bg-slate-200 border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
-          />
-          <button
-            onClick={addQuote}
-            className="p-3 rounded-lg transition-all hover:bg-blue-600 bg-blue-500 text-white font-bold shadow-lg"
-          >
-            Add Quote
-          </button>
-        </div>
-      </div>
-
-      {/* Display user-added quotes */}
-      <div className="p-6 mt-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-xl font-semibold mb-4">Visitors memos:</h2>
-        <div className="flex flex-col items-center">
-          {userQuotes.length > 0 ? (
-            <ul className="space-y-4">
-              {userQuotes.map((quote) => (
-                <li
-                  key={quote.quoteId}
-                  className="w-full bg-blue-100 p-4 rounded-lg shadow-md flex flex-col items-center"
-                >
-                  <p className="font-bold text-lg text-center">{quote.quote}</p>
-                  <p className="text-xs text-gray-600 mt-1 italic">
-                    {quote.author}
-                  </p>
-                  {quote.userId === userId && (
-                    <button
-                      className="mt-3 px-4 py-2 text-xs text-red-500 font-semibold bg-red-100 hover:bg-red-200 rounded-full transition-all"
-                      onClick={() => deleteQuote(quote.quoteId)}
+        {/* Display user-added quotes */}
+        <div className="p-6 mt-6 bg-white rounded-lg shadow-lg">
+          <h2 className="text-xl font-semibold mb-4">Visitors memos:</h2>
+          <div className="flex flex-col items-center">
+            {userQuotes.length > 0 ? (
+              <Fade>
+                <ul className="space-y-4">
+                  {userQuotes.map((quote) => (
+                    <li
+                      key={quote.quoteId}
+                      className="w-full bg-blue-100 p-4 rounded-lg shadow-md flex flex-col items-center"
                     >
-                      Delete
-                    </button>
-                  )}
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p className="text-gray-500">No new quotes added yet.</p>
-          )}
+                      <p className="font-bold text-lg text-center">
+                        {quote.quote}
+                      </p>
+                      <p className="text-xs text-gray-600 mt-1 italic">
+                        {quote.author}
+                      </p>
+                      {quote.userId === userId && (
+                        <button
+                          className="mt-3 px-4 py-2 text-xs text-red-500 font-semibold bg-red-100 hover:bg-red-200 rounded-full transition-all"
+                          onClick={() => deleteQuote(quote.quoteId)}
+                        >
+                          Delete
+                        </button>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </Fade>
+            ) : (
+              <p className="text-gray-500">Loading memos...</p>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Fade>
   );
 };
 
