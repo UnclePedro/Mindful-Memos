b0591513858c1617216f2fc582c7b868e13f75f5
Uncle Pedro <peterforsyth10@gmail.com>
2024-10-03 17:02:34 +1000

    Tweak styling

--- src/components/QuoteGenerator.tsx
+++ src/components/QuoteGenerator.tsx
@@ -109,7 +109,7 @@ const QuoteGenerator = () => {
 
         {editUserId && (
           <Fade duration={300} triggerOnce={true}>
-            <div className="fixed inset-0 bg-black bg-opacity-65 flex justify-center items-center z-50">
+            <div className="fixed inset-0 bg-black bg-opacity-65 flex justify-center h-screen items-center z-50">
               <div className="bg-white p-6 rounded-lg shadow-lg relative">
                 <button
                   onClick={() => setEditUserId(false)}
