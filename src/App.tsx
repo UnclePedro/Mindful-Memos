395f218ba43b05b020f14f7f0ba7e003372d25a9
Uncle Pedro <peterforsyth10@gmail.com>
2024-10-09 12:28:24 +1000

    Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import QuoteGenerator from "./components/QuoteGenerator";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <div className="m-4">
         <h1 className="text-4xl font-bold">Mindful Memos</h1>
         <p>Share an anecdote...</p>
